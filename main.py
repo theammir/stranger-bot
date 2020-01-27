14541855715651212877bc9c5a563a9658e145e3
TheAmmiR <yakorol2015@gmail.com>
2020-01-27 19:26:47 +0200

    master

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import discord
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix = "u")
+
 @bot.command
 async def ping(ctx):
-    ctx.send("Pong!")
+    await ctx.send("Pong!")
 
-bot.run('LFCOKZWih7k-KaWMJY5v0TiNDg-JXW6T')
+bot.run('NjcwNjkyOTAwNTkzNTk4NTMw.Xi8ckg.cSsBtA8xcNoWjHXodgJdV-tDV4Q')
