fac59aabac01c104c3a6d2111ccd5d4c32cdf9f4
TheAmmiR <yakorol2015@gmail.com>
2020-01-27 19:51:04 +0200

    v2

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ from discord.ext import commands
 
 bot = commands.Bot(command_prefix = "u")
 
-@bot.command
-async def ping(ctx):
-    await ctx.send("Pong!")
+@bot.event
+async def on_message(message):
+    channel_id = message.channel.id
+    channel = bot.get_channel(channel_id)
+    if (message.content == "uping"):
+        await channel.send("Upong, " + str(message.author) + "!")
 
 bot.run('NjcwNjkyOTAwNTkzNTk4NTMw.Xi8ckg.cSsBtA8xcNoWjHXodgJdV-tDV4Q')
